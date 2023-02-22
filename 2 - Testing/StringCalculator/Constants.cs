4e3745ed3042100121195ec2200c926d5b74361a
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-02-22 21:34:10 +0200

    Added partial solution
    - solved tasks 1-4
    - add method can handle a one-character different delimiter

---
+++ 2 - Testing/StringCalculator/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Constants
+    {
+        public static readonly int DELIMITER_SIZE = 10;
+        public static readonly int SINGLE_DELIMITER_INDEX = 2;
+    }
+}
