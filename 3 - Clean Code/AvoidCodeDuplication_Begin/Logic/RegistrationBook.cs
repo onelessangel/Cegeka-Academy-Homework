a877d6d1ba5292ef375e5aa3c2ce11d937935d63
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-02-24 15:00:45 +0200

    Added session 3 skel

---
+++ 3 - Clean Code/AvoidCodeDuplication_Begin/Logic/RegistrationBook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AvoidCodeDuplication_Begin.Logic
+{
+    public class RegistrationBook
+    {
+        private int nextId;
+        private List<Person> persons;
+
+        public RegistrationBook()
+        {
+            persons = new List<Person>();
+            nextId = 0;
+        }
+
+        public void RegisterPerson(string firstName, string lastName, Gender gender)
+        {
+            var person = new Person(nextId++, firstName, lastName, gender);
+            persons.Add(person);
+        }
+
+        public int CountFemales()
+        {
+            return CountByGender(Gender.Female);
+        }
+
+        private int CountByGender(Gender gender)
+        {
+            return persons.Count(person => person.Gender == gender);
+        }
+
+        public int CountMales()
+        {
+            return CountByGender(Gender.Male);
+        }
+    }
+}
---
+++ 3 - Clean Code/MeaningfulNames_End/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MeaningfulNames_End
+{
+    class Program
+    {
+        //public static double ComputeAverage(List<int> numbers)
+        //{
+        //    return numbers.Count == 0 ? 0 : numbers.Average();
+        //}
+
+        public static double ComputeAverage(List<int> numbers)
+        {
+            double sum = 0;
+            int howManyNumbers = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+                howManyNumbers++;
+            }
+
+            var isListEmpty = howManyNumbers == 0;
+            if (isListEmpty)
+            {
+                return 0;
+            }
+            else
+            {
+                return sum / howManyNumbers;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Average is: {ComputeAverage(new List<int>())}");
+            Console.WriteLine($"Average is: {ComputeAverage(new List<int> { 1, 2, 3, 4, 5 })}");
+            Console.WriteLine($"Average is: {ComputeAverage(new List<int> { 10, -2, -13, 4, 9 })}");
+        }
+    }
+}
