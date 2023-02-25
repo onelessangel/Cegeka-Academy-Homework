febf23b671a7429cca4311e3d52c371f92ab038f
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-02-25 13:56:31 +0200

    Refactored TennisGame2

--- 3 - Clean Code/Exercises/tennis/TennisGame2.cs
+++ 3 - Clean Code/Exercises/tennis/TennisGame2.cs
@@ -2,142 +2,135 @@ using System;
 
 namespace Tennis
 {
-  public class TennisGame2 : TennisGame
-  {
-    public int P1point = 0;
-    public int P2point = 0;
-    
-    public string P1res = "";
-    public string P2res = "";
-    private string player1Name;
-    private string player2Name;
+	public class TennisGame2 : TennisGame
+	{
+		private Player2 player1 = new Player2();
+		private Player2 player2 = new Player2();
 
-    public TennisGame2 (string player1Name, string player2Name)
-    {
-      this.player1Name = player1Name;
-      this.player2Name = player2Name;
-    }
+		private const string ZERO_POINTS = "Love";
+		private const string ONE_POINT = "Fifteen";
+		private const string TWO_POINTS = "Thirty";
+		private const string THREE_POINTS = "Forty";
+		private const string DEUCE = "Deuce";
+		private const string SCORE_EQUALITY = "-All";
 
-    public string GetScore(){
-      string score = "";
-      if (P1point == P2point && P1point < 4)
-      {
-        if (P1point==0)
-          score = "Love";
-        if (P1point==1)
-          score = "Fifteen";
-        if (P1point==2)
-          score = "Thirty";
-        if (P1point==3)
-          score = "Forty";
-        score += "-All";
-      }
-      if (P1point==P2point && P1point>3)
-        score = "Deuce";
-      
-      if (P1point > 0 && P2point==0)
-      {
-        if (P1point==1)
-          P1res = "Fifteen";
-        if (P1point==2)
-          P1res = "Thirty";
-        if (P1point==3)
-          P1res = "Forty";
-        
-        P2res = "Love";
-        score = P1res + "-" + P2res;
-      }
-      if (P2point > 0 && P1point==0)
-      {
-        if (P2point==1)
-          P2res = "Fifteen";
-        if (P2point==2)
-          P2res = "Thirty";
-        if (P2point==3)
-          P2res = "Forty";
-        
-        P1res = "Love";
-        score = P1res + "-" + P2res;
-      }
-      
-      if (P1point>P2point && P1point < 4)
-      {
-        if (P1point==2)
-          P1res="Thirty";
-        if (P1point==3)
-          P1res="Forty";
-        if (P2point==1)
-          P2res="Fifteen";
-        if (P2point==2)
-          P2res="Thirty";
-        score = P1res + "-" + P2res;
-      }
-      if (P2point>P1point && P2point < 4)
-      {
-        if (P2point==2)
-          P2res="Thirty";
-        if (P2point==3)
-          P2res="Forty";
-        if (P1point==1)
-          P1res="Fifteen";
-        if (P1point==2)
-          P1res="Thirty";
-        score = P1res + "-" + P2res;
-      }
-      
-      if (P1point > P2point && P2point >= 3)
-      {
-        score = "Advantage player1";
-      }
-      
-      if (P2point > P1point && P1point >= 3)
-      {
-        score = "Advantage player2";
-      }
-      
-      if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-      {
-        score = "Win for player1";
-      }
-      if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-      {
-        score = "Win for player2";
-      }
-      return score;
-    }
-    
-    public void SetP1Score(int number){
-      
-      for (int i = 0; i < number; i++)
-      {
-        P1Score();
-      }
-      
-    }
-    
-    public void SetP2Score(int number){
-      
-      for (int i = 0; i < number; i++)
-      {
-        P2Score();
-      }
-      
-    }
-    
-    public void P1Score(){
-      P1point++;
-    }
-    
-    public void P2Score(){
-      P2point++;
-    }
-    
-    public void WonPoint(string player) {
-      if (player == "player1")
-        P1Score();
-      else
-        P2Score();
-    }
+		public TennisGame2(string playerName1, string playerName2)
+		{
+			player1.Name = playerName1;
+			player2.Name = playerName2;
+		}
 
-  }
+		public string GetScore()
+		{
+			string score = string.Empty;
+
+			ComputePlayerResult(player1);
+			ComputePlayerResult(player2);
+
+			if (PlayersHaveSamePoints())
+			{
+				if (player1.Points < 4)
+				{
+					score = player1.Result;
+					score += SCORE_EQUALITY;
+				}
+				else if (player1.Points > 3)
+				{
+					score = DEUCE;
+				}
+			}
+			else if (PlayerWithZeroPoints() || ScoreIsMixed())
+			{
+				score = player1.Result + "-" + player2.Result;
+			}
+			else if (CheckPlayerWin(player1, player2) ||
+					 CheckPlayerWin(player2, player1))
+			{
+				Player2 playerWithMorePoints = GetPlayerWithMorePoints(player1, player2);
+				score = "Win for " + playerWithMorePoints.Name;
+			}
+			else if (CheckPlayerAdvantage(player1, player2) ||
+					 CheckPlayerAdvantage(player2, player1))
+			{
+				Player2 playerWithMorePoints = GetPlayerWithMorePoints(player1, player2);
+				score = "Advantage " + playerWithMorePoints.Name;
+			}
+
+			return score;
+		}
+
+		public void IncreasePlayerScore(Player2 player)
+		{
+			player.Points++;
+		}
+
+		public void WonPoint(string player)
+		{
+			if (player == "player1")
+			{
+				IncreasePlayerScore(player1);
+			}
+			else
+			{
+				IncreasePlayerScore(player2);
+			}
+		}
+
+		public void ComputePlayerResult(Player2 player)
+		{
+			if (player.Points == 0)
+			{
+				player.Result = ZERO_POINTS;
+			}
+			else if (player.Points == 1)
+			{
+				player.Result = ONE_POINT;
+			}
+			else if (player.Points == 2)
+			{
+				player.Result = TWO_POINTS;
+			}
+			else if (player.Points == 3)
+			{
+				player.Result = THREE_POINTS;
+			}
+		}
+
+		private bool PlayersHaveSamePoints()
+		{
+			return player1.Points == player2.Points;
+		}
+
+		public bool PlayerWithZeroPoints()
+		{
+			return player1.Points == 0 || player2.Points == 0;
+		}
+
+		public bool ScoreIsMixed()
+		{
+			return
+				((player1.Points > player2.Points) && (player1.Points < 4) ||
+				(player2.Points > player1.Points) && (player2.Points < 4));
+		}
+
+		public Player2 GetPlayerWithMorePoints(Player2 player1, Player2 player2)
+		{
+			return player1.Points > player2.Points ? player1 : player2;
+		}
+
+		public bool CheckPlayerAdvantage(Player2 playerWithAdvantage, Player2 playerWithoutAdvantage)
+		{
+			return
+				playerWithAdvantage.Points > playerWithoutAdvantage.Points &&
+				playerWithoutAdvantage.Points >= 3;
+		}
+
+		public bool CheckPlayerWin(Player2 playerWinning, Player2 playerLosing)
+		{
+			return
+				playerWinning.Points >= 4	&&
+				playerLosing.Points >= 0	&&
+				(playerWinning.Points - playerLosing.Points) >= 2;
+		}
+	}
 }
