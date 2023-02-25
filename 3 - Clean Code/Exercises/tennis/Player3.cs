0658377b53f1a5c7bfe99f5f812e62cbf32fb341
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-02-25 14:17:24 +0200

    Refactored TennisGame3

---
+++ 3 - Clean Code/Exercises/tennis/Player3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tennis
+{
+	public class Player3
+	{
+		public string Name { get; set; } = string.Empty;
+		public int Points { get; set; }
+	}
+}
--- 3 - Clean Code/Exercises/tennis/TennisGame3.cs
+++ 3 - Clean Code/Exercises/tennis/TennisGame3.cs
@@ -2,39 +2,68 @@ using System;
 
 namespace Tennis
 {
-  public class TennisGame3 : TennisGame
-  {
-    private int p2;
-    private int p1;
-    private string p1N;
-    private string p2N;
+	public class TennisGame3 : TennisGame
+	{
+		private Player3 player1 = new Player3();
+		private Player3 player2 = new Player3();
 
-    public TennisGame3 (string player1Name, string player2Name)
-    {
-      this.p1N = player1Name;
-      this.p2N = player2Name;
-    }
+		private const string ZERO_POINTS = "Love";
+		private const string ONE_POINT = "Fifteen";
+		private const string TWO_POINTS = "Thirty";
+		private const string THREE_POINTS = "Forty";
+		private const string DEUCE = "Deuce";
+		private const string SCORE_EQUALITY = "-All";
 
-    public string GetScore() {
-      string s;
-      if (p1 < 4 && p2 < 4) {
-        string[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-        s = p[p1];
-        return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-      } else {
-        if (p1 == p2)
-          return "Deuce";
-        s = p1 > p2 ? p1N : p2N;
-        return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
-      }
-    }
-    
-    public void WonPoint(string playerName) {
-      if (playerName == "player1")
-        this.p1 += 1;
-      else
-        this.p2 += 1;
-    }
+		public TennisGame3(string playerName1, string playerName2)
+		{
+			player1.Name = playerName1;
+			player2.Name = playerName2;
+		}
 
-  }
+		public string GetScore()
+		{
+			string score;
+
+			if (player1.Points < 4 && player2.Points < 4)
+			{
+				string[] scorePoints = new string[] { ZERO_POINTS, ONE_POINT, TWO_POINTS, THREE_POINTS };
+
+				score = scorePoints[player1.Points];
+
+				if (player1.Points == player2.Points)
+				{
+					score += SCORE_EQUALITY;
+				}
+				else
+				{
+					score += "-" + scorePoints[player2.Points];
+				}
+
+				return score;
+			}
+
+			if (player1.Points == player2.Points)
+			{
+				return "Deuce";
+			}
+					
+			string playerWithMorePoints = player1.Points > player2.Points ? player1.Name : player2.Name;
+			score = ((player1.Points - player2.Points) * (player1.Points - player2.Points) == 1) ? "Advantage " : "Win for ";
+
+			return score + playerWithMorePoints;
+		}
+
+		public void WonPoint(string playerName)
+		{
+			if (playerName == "player1")
+			{
+				player1.Points++;
+			}
+			else
+			{
+				player2.Points++;
+			}
+		}
+
+	}
 }
