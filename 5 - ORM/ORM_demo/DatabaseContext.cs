583ebff8b953ee0fdda6b0aed66927cfb3aa5cf8
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-03-02 14:23:25 +0200

    Added skel

---
+++ 5 - ORM/ORM_demo/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_demo
+{
+	public class DatabaseContext : DbContext
+	{
+		public DbSet<Product> Products { get; set; }
+		public DbSet<OrderItem> OrderItems { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=DESKTOP-4HELTG1\\SQLEXPRESS; Database=ORM; Integrated Security=True;");
+		}
+	}
+}
