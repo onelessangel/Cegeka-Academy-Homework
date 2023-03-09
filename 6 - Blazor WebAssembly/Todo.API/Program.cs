2b4713ecbe9c5b9910c6de381e3245fb06320893
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-03-09 23:56:53 +0200

    Added Description for each Todo Item

---
+++ 6 - Blazor WebAssembly/Todo.API/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Owin.Cors;
+using Todo.API.DataProvider;
+using Todo.Shared;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ITodosProvider, TodosProvider>();
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/todos", async (ITodosProvider todosProvider) => 
+{
+    var list = await todosProvider.GetTodos();
+    return list;
+});
+
+app.MapPost("/todos", async (ToDoItem todo, ITodosProvider todosProvider) => 
+{
+    await todosProvider.Add(todo);
+});
+
+app.MapPut("/todos/{id}", async (ToDoItem todo, ITodosProvider todosProvider) => 
+{
+    await todosProvider.Edit(todo);
+});
+
+app.MapGet("/todos/{id}", async (Guid id, ITodosProvider todosProvider) =>
+{
+    var todoItem = await todosProvider.GetTodo(id);
+    return todoItem;
+});
+
+app.MapDelete("/todos/{id}", async (Guid id, ITodosProvider todosProvider) => {
+    await todosProvider.Remove(id);
+});
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ 6 - Blazor WebAssembly/TodoBlazorWasm/Pages/Index.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+using Todo.Shared;
+
+namespace TodoBlazorWasm.Pages
+{
+    public partial class Index
+    {
+        public List<ToDoItem> Items { get; set; } = new List<ToDoItem>();
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var array = await HttpClient.GetFromJsonAsync<ToDoItem[]>("/todos");
+            Items = new List<ToDoItem>(array);
+        }
+
+        protected async Task DeleteTodo(ToDoItem item)
+        {
+            await HttpClient.DeleteAsync($"/todos/{item.Id}");
+            var array = await HttpClient.GetFromJsonAsync<ToDoItem[]>("/todos");
+            Items = new List<ToDoItem>(array);
+            this.StateHasChanged();
+        }
+    }
+}
