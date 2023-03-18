d3e9255bfd63afc67807a56e0620fd2b4ee75998
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-03-19 01:21:24 +0200

    Added Description
    - added migration in Todo.API.DataProvider
    - added optional text area for Description

---
+++ 7 - Blazor 2/Todo.API.DataProvider/MappingUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.API.DataProvider.Entities;
+using Todo.Shared;
+
+namespace Todo.API.DataProvider
+{
+    internal static class MappingUtils
+    {
+        public static TodoItemModel ModelFromEntity(TodoItem entity)
+        {
+            return new TodoItemModel
+            {
+                Id = entity.PublicId,
+                Title = entity.Title,
+                Description = entity.Description,
+                IsDone = entity.IsDone
+            };
+        }
+    }
+}
