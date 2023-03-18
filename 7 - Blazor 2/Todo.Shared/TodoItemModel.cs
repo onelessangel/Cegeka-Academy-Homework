d80e7f0bb510dd643f5b9db0287a615c41c3bf17
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-03-19 01:21:24 +0200

    Added Description
    - added migration in Todo.API.DataProvider
    - added optional text area for Description

---
+++ 7 - Blazor 2/Todo.Shared/TodoItemModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Shared
+{
+    public class TodoItemModel
+    {
+		public Guid? Id { get; set; }
+
+		[Required]
+		[MaxLength(100, ErrorMessage = "Please make the title shorter")]
+		public string Title { get; set; } = string.Empty;
+
+		[MaxLength(1000, ErrorMessage = "Please shorten the description")]
+		public string Description { get; set; } = string.Empty;
+
+		public bool IsDone { get; set; }
+		
+        public TodoItemModel()
+        {
+        }
+
+        public TodoItemModel(Guid id, string title = "", string description = "", bool isDone = false)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            IsDone = isDone;
+        }
+    }
+}
